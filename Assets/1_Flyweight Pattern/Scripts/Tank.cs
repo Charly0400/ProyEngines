8db39ddad59419f71e3d58f83f91ae6462cd7982
Charly0400 <Carlaz020400@gmail.com>
2024-09-15 23:17:38 -0600

    .

--- Assets/1_Flyweight Pattern/Scripts/Tank.cs
+++ Assets/1_Flyweight Pattern/Scripts/Tank.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,18 +10,17 @@ public class Tank : MonoBehaviour
 
     private Image _spriteImg;
     private Image _FlagImg;
-    private Text _nameText;
+    private TextMeshProUGUI _nameText;
 
 
     private void Awake()
     {
         _spriteImg = transform.Find("Sprite").GetComponent<Image>();
         _FlagImg = transform.Find("Flag").GetComponent<Image>();
-        _nameText = transform.Find("ModelTxt").GetComponent <Text>();
+        _nameText = transform.Find("ModelTxt").GetComponent <TextMeshProUGUI>();
 
         _spriteImg.sprite = dataSO.sprite;
         _FlagImg.sprite = dataSO.nation;
-        Debug.Log(_nameText.text);
         _nameText.text = dataSO.name;
     }
 
